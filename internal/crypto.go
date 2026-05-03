c862e121e1c58a44ab88f669571c022d6ed23eab
Anton <antongozlan@hotmail.com>
2026-05-03 18:00:26 +0200

    implement encrypt/decrypt functions

--- internal/crypto.go
+++ internal/crypto.go
@@ -1,6 +1,9 @@
 package internal
 
 import (
+	"crypto/rand"
+	"crypto/aes"
+
 	"golang.org/x/crypto/argon2"
 )
 
@@ -9,5 +12,48 @@ func DeriveKey(password []byte, salt[]byte) []byte  {
 	return key
 }
 
-// func encrypt
-// func decrypt
+func encrypt(masterKey []byte, password string) ([]byte, []byte) {
+	
+	// Create "chiffreur" AES-GCM with MasterKey
+	block, err := aes.NewCypher(masterKey)
+	if err != nil {
+		panic(err.Error())
+	}
+	aescgm, err := cipher.NewCGM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Generate a nonce
+	nonce:= make([]byte, aescgm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	// Encrypt : use nonce and string in chiffreur
+	ciphertext := aescgm.Seal(nil, nonce, password, nil)
+	
+	// Return nonce and ciphertext
+	return nonce, ciphertext
+}
+
+func decrypt(masterKey []byte, ciphertext []byte, nonce []byte) string {
+	
+	// Create "chiffreur" AES-GCM with MasterKey
+	block, err := aes.NewCypher(masterKey)
+	if err != nil {
+		panic(err.Error())
+	}
+	aescgm, err := cipher.NewCGM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Decrypt password
+	password, err := aescgm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	return password
+}
